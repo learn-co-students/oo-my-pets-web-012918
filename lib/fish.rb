b2c6216248d329cd493d94d0c9a229c3ebc05c62
github username <github email address>
2018-02-01 22:15:02 -0500

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,18 @@
 class Fish
-  # code goes here
+
+  attr_accessor :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
